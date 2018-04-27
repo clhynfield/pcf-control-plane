4b528b469cb48bf6baedb069e3eee32c055f0d01
Clayton Hynfield <clayton@hynfield.org>
2018-04-27 15:12:53 +0000

    Refactor Concourse tests
    - Remove dependency on IaaS-specific IP address
    - Remove dependency on static credentials
    - Use smaller container image
    - Explicitly test triggered build

--- provision-concourse/test-concourse.sh
+++ provision-concourse/test-concourse.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
 
-fly login -t mgmt -c https://10.193.163.17
+: "${BBL_STATE_DIRECTORY:=../bbl-state}"
+: "${BBL_ENV_NAME:=control-plane}"
 
-fly -t mgmt sp -p test -c test_pipeline.yml --non-interactive
-fly -t mgmt unpause-pipeline -p test
-fly -t mgmt trigger-job -j test/hello-world
+cred_path="/bosh-$BBL_ENV_NAME/concourse"
+
+credhub_config="$HOME/.credhub/config.json"
+if [ -f "$credhub_config" ]; then
+    rm "$credhub_config"
+fi
+
+eval "$(bbl print-env)"
+
+concourse_username="$(credhub get --name "$cred_path/atc_basic_auth" --output-json | jq --raw-output '.value.username')"
+concourse_password="$(credhub get --name "$cred_path/atc_basic_auth" --output-json | jq --raw-output '.value.password')"
+concourse_url="https://$(
+    bosh --deployment concourse --json instances \
+        | jq --raw-output 'limit(1;
+            .Tables[0].Rows[]|select(.instance|startswith("web")))|.ips'
+    ):4443"
+concourse_ca_cert="/tmp/test-concourse-ca-cert-$$"
+credhub get --name "$cred_path/concourse-ca" --output-json | jq --raw-output '.value.certificate' > "$concourse_ca_cert"
+
+fly login \
+    --target test \
+    --concourse-url "$concourse_url" \
+    --username "$concourse_username" \
+    --password "$concourse_password" \
+    --ca-cert "$concourse_ca_cert"
+
+fly --target test set-pipeline \
+    --pipeline simple \
+    --config test-pipeline-simple.yml \
+    --non-interactive
+
+fly --target test unpause-pipeline \
+    --pipeline simple
+
+build="$(
+    fly --target test trigger-job \
+        --job simple/hello-world \
+        --verbose \
+        2>&1 \
+        | grep job_name \
+        | jq --raw-output '.name'
+    )"
+
+fly --target test watch \
+    --job simple/hello-world \
+    --build "$build"
