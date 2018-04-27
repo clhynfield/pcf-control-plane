b6982a451f175ab6b32fc7ab2e2e5fc27ef64274
Clayton Hynfield <clayton@hynfield.org>
2018-04-27 13:09:11 +0000

    Refactor script to target Concourse CredHub
    Remove dependency on IaaS-specific IP address

--- provision-concourse/target-concourse-credhub.sh
+++ provision-concourse/target-concourse-credhub.sh
@@ -1,16 +1,30 @@
 : "${BBL_STATE_DIRECTORY:=../bbl-state}"
 : "${BBL_ENV_NAME:=control-plane}"
 
+cred_path="/bosh-$BBL_ENV_NAME/concourse"
+credhub_config="$HOME/.credhub/config.json"
+
 eval "$(bbl print-env)"
 
-rm "$HOME/.credhub/config.json"
+if [ -f "$credhub_config" ]; then
+    rm "$credhub_config"
+fi
 
+CREDHUB_CA_CERT="$(
+    credhub get -n $cred_path/atc_tls -j \
+        | jq -r '.value.ca'
+    )"
+CREDHUB_SECRET="$(
+    credhub get -n $cred_path/concourse_to_credhub_secret -j \
+        | jq -r '.value'
+    )"
+CREDHUB_SERVER="$(
+    bosh --deployment concourse --json instances \
+        | jq --raw-output 'limit(1;
+            .Tables[0].Rows[]|select(.instance|startswith("web")))|.ips'
+    ):8844"
+CREDHUB_CLIENT=concourse_to_credhub
 
-export CREDHUB_CLIENT=concourse_to_credhub
-export CREDHUB_CA_CERT="$(credhub get -n /bosh-$BBL_ENV_NAME/concourse/atc_tls -j | jq -r '.value.ca')"
-export CREDHUB_SECRET="$(credhub get -n /bosh-$BBL_ENV_NAME/concourse/concourse_to_credhub_secret -j | jq -r .value)"
-export CREDHUB_SERVER="10.39.28.15:8844"
+unset CREDHUB_USER CREDHUB_PASSWORD
+export CREDHUB_CLIENT CREDHUB_CA_CERT CREDHUB_SECRET CREDHUB_SERVER
 credhub api
-
-unset CREDHUB_USER
-unset CREDHUB_PASSWORD
